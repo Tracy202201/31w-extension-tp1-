b4b061c7b8619574c512591be44e2ef883befbb0
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2022-06-27 15:02:30 -0400

    ajouter js dans ficher carrousel.js

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,5 +1,16 @@
 (function () {
     console.log('début du carrousel');
+    let elmGalarie = document.querySelector(".galarie");
 
+    let elmGalarie__img = document.querySelectorAll('.galarie figure img');
+
+    console.log(elmGalarie__img);
+
+    for (const img of elmGalarie__img) {
+        img.addEventListener('mousedown', function () {
+            console.log(img.getAttribute('src'));
+        })
+        
+    }
 })()
 
