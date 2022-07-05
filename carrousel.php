f464f2291815dfd1f093f387cb648a9142759000
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2022-07-05 14:57:22 -0400

    ajouter des commentaires dur carrousel.php

--- carrousel.php
+++ carrousel.php
@@ -35,9 +35,13 @@ add_action('wp_enqueue_scripts', 'carrousel_31w_enqueue');
 function genere_carrousel()
 {
     /////////////////////////////////////// HTML
+    //le contenu du carrousel
     $contenu = '<div class="carrousel">';
+    //le bouton fermeture
     $contenu .= '<button class="carrousel__fermeture">X</button>';
+    //le conteneur d'images
     $contenu .= '<figure class ="carrousel__img></figure>';
+    //le forlulaire des radio bouton pour naviguer d'une image a l'autre
     $contenu .= '<form class ="carrousel__radio></form>';
     $contenu .= '</div><!--- fin du carrousel---!>';
     
