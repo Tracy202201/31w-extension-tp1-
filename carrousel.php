4e59af3a0e2ae31534da6c513576c72262b1f423
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2022-07-06 18:35:35 -0400

    ajouter readme.md dans l'extension carrousel

--- carrousel.php
+++ carrousel.php
@@ -13,20 +13,25 @@ function carrousel_31w_enqueue()
     $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
     $version_js = filemtime(plugin_dir_path(__FILE__) . "script/carrousel.js");
     //var_dump(__FILE__); die()
-    wp_enqueue_style(   'carrousel_31w_css', 
-                        plugin_dir_url( __FILE__)  . "style.css",
-                        array(),
-                        $version_css);
+    wp_enqueue_style(
+        'carrousel_31w_css',
+        plugin_dir_url(__FILE__)  . "style.css",
+        array(),
+        $version_css
+    );
 
-    wp_enqueue_script(  'carrousel_31w_js', 
-                        plugin_dir_url( __FILE__) . "script/carrousel.js",
-                        array(),
-                        $version_js,
-                        true);
+    wp_enqueue_script(
+        'carrousel_31w_js',
+        plugin_dir_url(__FILE__) . "script/carrousel.js",
+        array(),
+        $version_js,
+        true
+    );
 }
 add_action('wp_enqueue_scripts', 'carrousel_31w_enqueue');
 
-function genere_carrousel(){
+function genere_carrousel()
+{
     /////////////////////////////////////// HTML
     // Le conteneur du carrousel
     $contenu = '<div class="carrousel">';
@@ -37,7 +42,7 @@ function genere_carrousel(){
     // Le formulaire des radio bouton pour naviguer d'une image à l'autre
     $contenu .= '<form class="carrousel__radio"></form>';
     $contenu .= '</div> <!-- fin du carrousel -->';
-   
+
     return $contenu;
 }
 
