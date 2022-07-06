21cbb327b581f55b0cd475242abdd276566f0af8
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2022-07-06 18:19:08 -0400

    ajouter des function ouvrir dans carrousel.js

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,1 +1,20 @@
+(function(){
+    console.log('Début du carrousel')
+   
+    let elmGalerie = document.querySelector('.galerie')
+    let elmGalerieImg = elmGalerie.querySelectorAll("figure figure img")
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure')
+    let elmCarrousel__fermeture = document.querySelector('.carrousel__fermeture')
+    let elmCarrousel__radio = document.querySelector('.carrousel__radio')
+    console.log(elmGalerieImg[0].getAttribute('src'))
+    let index = 0
+    for (const img of elmGalerieImg){
+        ajouter_elmImg(img)
+        ajouter_elmRadio()
+        img.addEventListener('mousedown', function(){
+        elmCarrousel.classList.add('carrousel__ouvrir') 
+    }) 
+    }
 
+})()
