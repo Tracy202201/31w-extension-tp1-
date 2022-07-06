1492ebd941f2a78a5b92b921c92db42137ff8305
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2022-07-06 18:25:21 -0400

    modification de function pour ouvrir et fermer carrousel dans ficher carrousel.js

--- script/carrousel.js
+++ script/carrousel.js
@@ -17,27 +17,29 @@
     }) 
     }
 //////////////////////////////////////////////////////////  ajouter_elmimg
-function ajouter_elmImg(img){
-    let elmImg = document.createElement('img')
-    elmImg.classList.add('carrousel__figure__img')
-    elmImg.setAttribute('src', img.getAttribute('src'))
-    elmCarrousel__figure.appendChild(elmImg)
-    elmImg.dataset.index = index
-}
-function ajouter_elmRadio(){
-    let elmRadio = document.createElement('input')
-    elmRadio.setAttribute('type','radio')
-    elmRadio.setAttribute('name','radImg')
-    elmRadio.dataset.index = index
-    index = index+1
-    elmCarrousel__radio.appendChild(elmRadio)
-    elmRadio.addEventListener('mousedown', function(){
-        
-    elmCarrousel__figure.children[this.dataset.index].classList.add('carrousel__figure__img--activer')
-    })
-}
-elmCarrousel__fermeture.addEventListener('mousedown', function(){
-    elmCarrousel.classList.remove('carrousel__ouvrir') 
-})
+    function ajouter_elmImg(img){
+        let elmImg = document.createElement('img')
+        elmImg.classList.add('carrousel__figure__img')
+        elmImg.setAttribute('src', img.getAttribute('src'))
+        elmCarrousel__figure.appendChild(elmImg)
+        elmImg.dataset.index = index
+    }
 
+    function ajouter_elmRadio(){
+        let elmRadio = document.createElement('input')
+        elmRadio.setAttribute('type','radio')
+        elmRadio.setAttribute('name','radImg')
+        elmRadio.dataset.index = index
+        index = index+1
+        elmCarrousel__radio.appendChild(elmRadio)
+        elmRadio.addEventListener('mousedown', function(){
+            
+            elmCarrousel__figure.children[this.dataset.index].classList.add('carrousel__figure__img--activer')
+        
+        })
+    }
+  
+    elmCarrousel__fermeture.addEventListener('mousedown', function(){
+        elmCarrousel.classList.remove('carrousel__ouvrir') 
+    })
 })()
