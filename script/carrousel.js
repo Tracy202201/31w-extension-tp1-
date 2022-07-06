6e97c26fff8234f2e1ba06813945b6171713213c
Tracy202201 <e2195816@cmaisonneuve.qc.ca>
2022-07-06 18:20:07 -0400

    ajouter des function ajouter image dans carrousel.js

--- script/carrousel.js
+++ script/carrousel.js
@@ -16,5 +16,12 @@
         elmCarrousel.classList.add('carrousel__ouvrir') 
     }) 
     }
-
+//////////////////////////////////////////////////////////  ajouter_elmimg
+function ajouter_elmImg(img){
+    let elmImg = document.createElement('img')
+    elmImg.classList.add('carrousel__figure__img')
+    elmImg.setAttribute('src', img.getAttribute('src'))
+    elmCarrousel__figure.appendChild(elmImg)
+    elmImg.dataset.index = index
+}
 })()
